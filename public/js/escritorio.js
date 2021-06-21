41479f34fecce03bc147a5f8e13236e96674b8de
Alejandro Dubon <alejandrodubon88@gmail.com>
2021-06-21 17:20:52 -0600

    seccion 15 sockets terminado

--- public/js/escritorio.js
+++ public/js/escritorio.js
@@ -1,3 +1,57 @@
+const socket = io();
+const searchParams = new URLSearchParams(window.location.search);
 
 
-console.log('Escritorio HTML');
+
+//HTML
+const lblEscritorio = document.querySelector('h1');
+const btnAtender = document.querySelector('button');
+const lblTicket = document.querySelector('small');
+const alerta = document.querySelector('.alert');
+const lblPendientes = document.querySelector('#lblPendientes')
+
+if (!searchParams.has('escritorio')) {
+    window.location = 'index.html';
+    throw new Error('El escriotiro es obligatorio')
+}
+
+const escritorio = searchParams.get('escritorio');
+lblEscritorio.innerText = escritorio;
+alerta.style.display = 'none';
+
+
+socket.on('connect', () => {
+    // console.log('Conectado');
+    btnAtender.disabled = false;
+});
+
+socket.on('ultimo-ticket', (ultimo) => {
+});
+
+socket.on('tickets-pendientes', (pendientes) => {
+    if (pendientes === 0) {
+        alerta.style.display = '';
+    } else {
+        alerta.style.display = 'none';
+        lblPendientes.innerText = pendientes;
+    }
+});
+
+socket.on('disconnect', () => {
+    // console.log('Desconectado del servidor');
+    btnAtender.disabled = true;
+});
+
+
+btnAtender.addEventListener('click', () => {
+
+    socket.emit('atender-ticket', { escritorio }, ({ok, ticket, msg}) => {
+        if (!ok) {
+            lblTicket.innerHTML = 'Nadie';
+            return alerta.style.display = '';
+        }
+
+        lblTicket.innerHTML = 'Ticket ' + ticket.numero;
+    });
+
+});
--- public/js/nuevo-ticket.js
+++ public/js/nuevo-ticket.js
@@ -1,3 +1,31 @@
-
+const $lblNuevoTicket = document.querySelector('#lblNuevoTicket');
+const $button = document.querySelector('button');
 
 console.log('Nuevo Ticket HTML');
+const socket = io();
+
+socket.on('connect', () => {
+    // console.log('Conectado');
+    $button.disabled = false;
+});
+
+socket.on('ultimo-ticket', (ultimo) => {
+    $lblNuevoTicket.innerText = `Ultimo ticket: ${ultimo}`;
+});
+
+
+socket.on('disconnect', () => {
+    // console.log('Desconectado del servidor');
+    $button.disabled = true;
+});
+
+
+$button.addEventListener( 'click', () => {
+
+    
+    socket.emit( 'siguiente-ticket', null, ( ticket ) => {
+        console.log('Desde el server: ', ticket );
+        $lblNuevoTicket.innerText = ticket;
+    });
+
+});
